18f5ebf195f4c7cba5e0d77c25ac91bce2385a7f
= <=>
2023-03-10 17:22:48 +0100

    Commit Inicial

---
+++ src/main/java/com/peterfonkel/webMagazine/WebMagazineApplication.java
@@ -0,0 +1,13 @@
+package com.peterfonkel.webMagazine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebMagazineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebMagazineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/peterfonkel/webMagazine/WebMagazineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peterfonkel.webMagazine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebMagazineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
