4a3f7edcf725682249eb13a1e6d3058f29b30a57
Rafa_Reche <rechus@hotmail.com>
2023-04-01 20:46:25 +0200

    lista de productos hecha

--- WebMagazineFront/src/app/ecommerce/components/product-item/product-item.component.ts
+++ WebMagazineFront/src/app/ecommerce/components/product-item/product-item.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Product } from '../../models/product';
+import { MessageService } from '../../service/message.service';
 
 @Component({
   selector: 'app-product-item',
   templateUrl: './product-item.component.html',
-  styleUrls: ['./product-item.component.css']
+  styleUrls: ['./product-item.component.css'],
 })
 export class ProductItemComponent {
+  @Input()
+  product!: Product;
 
+  constructor(private messageService: MessageService) {}
+
+  addToCart(): void {
+    this.messageService.sendMessage(this.product);
+  }
 }
--- WebMagazineFront/src/app/ecommerce/service/product.service.ts
+++ WebMagazineFront/src/app/ecommerce/service/product.service.ts
@@ -1,9 +1,58 @@
 import { Injectable } from '@angular/core';
+import { Product } from '../models/product';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
+  //hardcoded para abreviar, pero estos datos de los productos deben venir del backend
+  products: Product[] = [
+    new Product(
+      1,
+      'Macbook Pro',
+      'descripcion',
+      19,
+      'https://m.media-amazon.com/images/I/710-zIN6mML._AC_SL1500_.jpg'
+    ),
+    new Product(
+      2,
+      'Samsung Galaxy Book Go',
+      'descripcion',
+      35,
+      'https://m.media-amazon.com/images/I/71WqmMpbo3L._AC_SL1500_.jpg'
+    ),
+    new Product(
+      3,
+      'Asus F415EA',
+      'descripcion',
+      46,
+      'https://m.media-amazon.com/images/I/71SbYCE7O+L._AC_SL1500_.jpg'
+    ),
+    new Product(
+      4,
+      'Acer Aspire',
+      'descripcion',
+      59,
+      'https://m.media-amazon.com/images/I/71qTAboBTsL._AC_SL1500_.jpg'
+    ),
+    new Product(
+      5,
+      'Surface Laptop 4',
+      'descripcion',
+      14,
+      'https://m.media-amazon.com/images/I/71Sdk9Eru6S._AC_SL1500_.jpg'
+    ),
+    new Product(
+      6,
+      'Lenovo IdeaPad 3',
+      'descripcion',
+      7,
+      'https://m.media-amazon.com/images/I/71-lKa3SBkL._AC_SL1500_.jpg'
+    ),
+  ];
+  constructor() {}
 
-  constructor() { }
+  getProducts(): Product[] {
+    return this.products;
+  }
 }
