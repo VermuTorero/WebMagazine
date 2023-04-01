342000e48a95253b24a8ad2ff93e9c774297b21f
Rafa_Reche <rechus@hotmail.com>
2023-04-01 19:59:34 +0200

    estructura core

--- WebMagazineFront/src/app/app-routing.module.ts
+++ WebMagazineFront/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PrincipalEcommerceComponent } from './ecommerce/components/principal-ecommerce/principal-ecommerce.component';
+import { MainComponent } from './core/shell/main/main.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: MainComponent},
+  {path: 'ecommerce', component: PrincipalEcommerceComponent},
+  {path: '**', redirectTo: '', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
