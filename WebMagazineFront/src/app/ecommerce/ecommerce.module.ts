317ceb68ee6fed1bc1cdb920c8843003e7a432ad
Rafa_Reche <rechus@hotmail.com>
2023-04-01 20:46:53 +0200

    lista de productos

--- WebMagazineFront/src/app/ecommerce/ecommerce.module.ts
+++ WebMagazineFront/src/app/ecommerce/ecommerce.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import {  NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CartComponent } from './components/cart/cart.component';
 import { CartItemComponent } from './components/cart-item/cart-item.component';
@@ -19,7 +19,7 @@ import { PrincipalEcommerceComponent } from './components/principal-ecommerce/pr
     PrincipalEcommerceComponent
   ],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+  ],
 })
 export class EcommerceModule { }
