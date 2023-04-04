0b07034085f6db2facb592b71788238b4433ec1d
Rafa_Reche <rafareche84@gmail.com>
2023-04-04 22:55:26 +0200

    Entidades producto y seccion

---
+++ src/main/java/com/peterfonkel/webMagazine/repositories/SeccionDAO.java
@@ -0,0 +1,15 @@
+package com.peterfonkel.webMagazine.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import com.peterfonkel.webMagazine.entities.Seccion;
+
+@RepositoryRestResource(path = "secciones", itemResourceRel = "seccion", collectionResourceRel = "secciones")
+public interface SeccionDAO extends JpaRepository<Seccion, String>{
+    
+    @Override
+	List<Seccion> findAll();
+}
