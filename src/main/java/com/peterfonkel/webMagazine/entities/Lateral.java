1672bde95d910cd63bc5c762abe4f86201a37398
PeterFonkel <peterfonkel@gmail.com>
2023-05-09 12:01:07 +0200

    id auto

--- src/main/java/com/peterfonkel/webMagazine/entities/Lateral.java
+++ src/main/java/com/peterfonkel/webMagazine/entities/Lateral.java
@@ -1,6 +1,8 @@
 package com.peterfonkel.webMagazine.entities;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
@@ -8,6 +10,7 @@ public class Lateral {
 	
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	Long id;
 	private String htmlPodcast;
 	private String htmlTwitter;
