e5a31084653976c6be20cfc6284d209eb15ee0a4
= <=>
2023-05-14 13:06:24 +0200

    update

--- src/main/java/com/peterfonkel/webMagazine/repositories/PublicacionDAO.java
+++ src/main/java/com/peterfonkel/webMagazine/repositories/PublicacionDAO.java
@@ -2,9 +2,10 @@ package com.peterfonkel.webMagazine.repositories;
 
 import java.util.List;
 
-import org.springframework.data.jdbc.repository.query.Query;
+
+//import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
+//import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.peterfonkel.webMagazine.entities.Publicacion;
@@ -22,7 +23,7 @@ public interface PublicacionDAO extends JpaRepository<Publicacion, Long>{
 	List<Publicacion> findByLugar_LugarNombreAndIdNot(String lugarNombre, Long id);
 	List<Publicacion> findByCategoria_categoriaNombre(String categoriaNombre);
 	Publicacion findByTitulo(String titulo);
-	@Query("SELECT p FROM Publicacion p WHERE LOWER(p.titulo) LIKE LOWER(CONCAT('%', :palabra, '%')) OR LOWER(p.subtitulo) LIKE LOWER(CONCAT('%', :palabra, '%'))")
-	List<Publicacion> buscarPorPalabra(@Param("palabra") String palabra);
+//	@Query("SELECT p FROM Publicacion p WHERE LOWER(p.titulo) LIKE LOWER(CONCAT('%', :palabra, '%')) OR LOWER(p.subtitulo) LIKE LOWER(CONCAT('%', :palabra, '%'))")
+//	List<Publicacion> buscarPorPalabra(@Param("palabra") String palabra);
 
 }
