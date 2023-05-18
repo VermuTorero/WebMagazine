5174733a5e9a13d75d19373814c564adbbca0ad5
Rafa_Reche <rafareche84@gmail.com>
2023-05-18 20:15:39 +0200

    ordenacion ecommerce ongoing

--- WebMagazineFront/src/app/ecommerce/components/product-list/product-list.component.ts
+++ WebMagazineFront/src/app/ecommerce/components/product-list/product-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
 import { Product } from '../../models/product';
 import { ProductService } from '../../service/product.service';
 
@@ -9,16 +9,54 @@ import { ProductService } from '../../service/product.service';
 })
 export class ProductListComponent {
   products: Product[] = [];
+  ordenActual: string = "precio"
+  ordenAscendente: boolean = true;
   constructor(private productService: ProductService) {}
 
   ngOnInit(): void {
     this.loadProducts();
   }
 
+
+  getProductosOrdenados(): Product[] {
+    if(this.ordenActual == "precio"){
+    return this.products.sort((a, b) => {
+      if (a.precio < b.precio) {
+        return this.ordenAscendente ? -1 : 1;
+      } else if (a.precio > b.precio) {
+        return this.ordenAscendente ? 1 : -1;
+      } else {
+        return 0;
+        
+      }
+    });
+    
+  }else{
+      return this.products.sort((a, b) => {
+        if (a.precio < b.precio) {
+          return this.ordenAscendente ? -1 : 1;
+        } else if (a.precio > b.precio) {
+          return this.ordenAscendente ? 1 : -1;
+        } else {
+          return 0;
+        }
+      });
+  }
+    }
+    
+
+
   loadProducts(): void {
     this.productService.getProducts().subscribe((res) =>{
       console.log(res);
       this.products = res;
+      this.getProductosOrdenados(); // Ordenar los productos al cargarlos inicialmente
     });
+  }
+
+  onChangeOrden(): void {
+    this.getProductosOrdenados(); // Ordenar los productos cuando se cambia la opción del select
+    this.products = [...this.products];
+    console.log(this.products);
   }
 }
