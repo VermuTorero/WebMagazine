d721b48ebe7c72da461fa07a11a0d1eb68a0d5e6
PeterFonkel <peterfonkel@gmail.com>
2023-05-28 18:52:09 +0200

    Abriendo endopints

--- src/main/java/com/peterfonkel/webMagazine/login/config/MyWebSecurity.java
+++ src/main/java/com/peterfonkel/webMagazine/login/config/MyWebSecurity.java
@@ -64,7 +64,7 @@ public class MyWebSecurity extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/oauth/**", "/api/publicaciones/search/**", "/api/publicaciones/**", 
+                .antMatchers("/oauth/**", "/api/publicaciones/search/**","/api/publicaciones/search/**/**" , "/api/publicaciones/**", 
                 		"/api/paginaEditables/**", "/api/imagenInicios/**", "/api/lugares/**", 
                 		"/api/categorias/**", "/api/tags/**", "/api/laterales/**", "/usuarios/search/**").permitAll()
                 .anyRequest().authenticated()
