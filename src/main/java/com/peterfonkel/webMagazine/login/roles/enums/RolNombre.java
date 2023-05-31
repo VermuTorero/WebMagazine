147485631f56695437a013f75c07eb3cc7080a8a
PeterFonkel <peterfonkel@gmail.com>
2023-05-31 20:49:56 +0200

    fix enum ROLE_WRITER

--- src/main/java/com/peterfonkel/webMagazine/login/roles/enums/RolNombre.java
+++ src/main/java/com/peterfonkel/webMagazine/login/roles/enums/RolNombre.java
@@ -2,7 +2,7 @@ package com.peterfonkel.webMagazine.login.roles.enums;
 
 public enum  RolNombre {
 	ROLE_ADMIN,
-	ROLE_WRITTER,
+	ROLE_WRITER,
     ROLE_USER_MEMBER,
     ROLE_USER_SUSCRIBED,
     ROLE_USER_REGISTERED
