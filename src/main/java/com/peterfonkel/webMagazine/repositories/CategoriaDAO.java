fda02452e1acf883c018ac0e1bc05671676a57a8
= <=>
2023-07-09 17:38:57 +0200

    securizando CategoriaDAO

--- src/main/java/com/peterfonkel/webMagazine/repositories/CategoriaDAO.java
+++ src/main/java/com/peterfonkel/webMagazine/repositories/CategoriaDAO.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
+import org.springframework.security.access.prepost.PreAuthorize;
 
 import com.peterfonkel.webMagazine.entities.Categoria;
 
@@ -13,4 +13,58 @@ public interface CategoriaDAO extends JpaRepository<Categoria, Long> {
 	@Override
 	List<Categoria> findAll();
 	Categoria findByCategoriaNombre(String categoriaNombre);
+	
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
+	@Override
+	<S extends Categoria> S save(S entity);
+	
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
+	@Override
+	void deleteById(Long id);
+	
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
+	@Override
+	void delete(Categoria entity);
+	
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
+	@Override
+	void deleteAllById(Iterable<? extends Long> ids);
+	
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
+	@Override
+	void deleteAll(Iterable<? extends Categoria> entities);
+	
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
+	@Override
+	void deleteAll();
+	
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
+	@Override
+	<S extends Categoria> List<S> saveAll(Iterable<S> entities);
+	
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
+	@Override
+	void flush();
+	
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
+	@Override
+	<S extends Categoria> S saveAndFlush(S entity);
+	
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
+	@Override
+	<S extends Categoria> List<S> saveAllAndFlush(Iterable<S> entities);
+	
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
+	@Override
+	void deleteAllInBatch(Iterable<Categoria> entities);
+	
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
+	@Override
+	void deleteAllByIdInBatch(Iterable<Long> ids);
+	
+	@PreAuthorize("hasRole('ROLE_ADMIN')")
+	@Override
+	void deleteAllInBatch();
+	
+	
 }
